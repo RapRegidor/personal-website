cd2f2a37ae4c3b03b8ea721b09f5aba9ce6829ae
RapRegidor <raplouis.regidor@stonybrook.edu>
2025-02-27 00:11:00 -0500

    animations

--- src/app/components/taskbar/taskbar.component.ts
+++ src/app/components/taskbar/taskbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { StartMenuComponent } from "../start-menu/start-menu.component";
 import { CommonModule } from '@angular/common';
+import {trigger, transition, state, animate, style} from '@angular/animations';
 
 @Component({
   selector: 'app-taskbar',
   imports: [StartMenuComponent, CommonModule],
   templateUrl: './taskbar.component.html',
-  styleUrl: './taskbar.component.css'
+  styleUrl: './taskbar.component.css',
+  animations:[
+    trigger('openClose',[
+      transition(':enter',[
+        style({height:0, opacity: 0}),
+        animate('100ms ease-out', style({height:'*', opacity: 1}))
+      ]),
+      transition(':leave',[
+        style({height:'*', opacity: 1}),
+        animate('100ms ease-in', style({height:0, opacity: 0}))
+      ]),
+    ])
+  ],
 })
 export class TaskbarComponent {
   isMenuOpen = false;
